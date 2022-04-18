a213522fbd4d76c2c24083664c01a9da9bcac993
mmickoff <mickmail2000@gmail.com>
2022-04-18 14:33:52 +0300

    Create Pack hw2 & Less021 Task021

---
+++ src/main/java/ru/gb/mmick/homework2/Lesson021.java
@@ -0,0 +1,7 @@
+package ru.gb.mmick.homework2;
+
+public class Lesson021 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ru/gb/mmick/homework2/Task021.java
@@ -0,0 +1,7 @@
+package ru.gb.mmick.homework2;
+
+public class Task021 {
+    public static void main(String[] args) {
+
+    }
+}
