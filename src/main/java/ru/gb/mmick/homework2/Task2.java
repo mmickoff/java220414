a76d70e4f37bbb5764fcbcc57e7e2a90cf4207a8
mmickoff <mickmail2000@gmail.com>
2022-04-20 01:05:54 +0300

    220420.0033.hw2.Task2&Exp.v05.Finish.01

--- src/main/java/ru/gb/mmick/homework2/Task2.java
+++ src/main/java/ru/gb/mmick/homework2/Task2.java
@@ -2,34 +2,32 @@ package ru.gb.mmick.homework2;
 
 
 public class Task2 {
-    public static void main(String[] args) {
-        System.out.println(within10and20(0, 12));
-        number1();
-        System.out.println(number2());
+    public static void main(String[] args) { // точка входа
+        System.out.println(within10and20(0, 12)); // метод пределов суммы
+        number1(5); //  метод проверки положительности числа
+        System.out.println(number2(5)); //
         String s = "Hello, world!"; // передаём значение ы в метод print1
-        ptintWordN(s, 6);
-        int y1 = 600;
-        leapYear(y1);
-        System.out.println(leapYear(y1));
+        ptintWordN(s, 6); //
+        int y1 = 600; //
+        leapYear(y1); //
+        System.out.println(leapYear(y1)); //
     }
 
-    public static boolean within10and20(int i, int i1) {
-        int sum = i + i1;
-        return (sum >= 10 && sum <= 20);
+    public static boolean within10and20(int i, int i1) { // метод прделов суммы
+        int sum = i + i1; // вычисление суммы
+        return (sum >= 10 && sum <= 20); // получение подтверждения верности выражения true
     }
 
-    public static void number1() {
-        int x = 5;
-        if (x < 0) {
+    public static void number1(int x) { // метод полжительности числа
+        if (x < 0) { // формула определения отриц
             System.out.println(x + " - число отрицательное");
-        } else {
+        } else { // иначе , если больше нуля
             System.out.println(x + " - число положительное");
         }
     }
 
-    public static boolean number2() {
-        int y = 5;
-        return (y > 0);
+    public static boolean number2(int y) { // метод подтверждения отрицтельности числа
+        return (y < 0); // возврат тру, если число отрицательное, и подразумевает обратное
     }
 
     public static void ptintWordN(String s, int n) { // создаём метод количества строк
@@ -37,7 +35,8 @@ public class Task2 {
             System.out.println(s); // печатаем строку
         }
     }
-    public static boolean leapYear(int y1) {
+    public static boolean leapYear(int y1) { //метод висококсность
         return (y1 % 4 == 0 & y1 % 100 != 0 | y1 % 400 == 0 );
+        // формула определения високосности - если да, то тру, если нет, то подразумевает фолс
     }
 }
