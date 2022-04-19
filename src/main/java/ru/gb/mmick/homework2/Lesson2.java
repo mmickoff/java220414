d290e14478743457548a691c406493a1148dfb57
mmickoff <mickmail2000@gmail.com>
2022-04-19 11:58:31 +0300

    220419 hw2 experimenty 01

--- src/main/java/ru/gb/mmick/homework2/Lesson2.java
+++ src/main/java/ru/gb/mmick/homework2/Lesson2.java
@@ -2,6 +2,19 @@ package ru.gb.mmick.homework2;
 
 public class Lesson2 {
     public static void main(String[] args) {
-
+        double r1 = 2.1, r2 = 3.12;
+        double s1 = calcSR(r1);
+        double s2 = calcSR(r2);
+        double sumS = s1 + s2;
+        print(sumS);
     }
+
+    public static double calcSR(double r) {
+        double s = 3.14 * r * r;
+        return s;
+    }
+    public static void print (double a){
+        System.out.println(a);
+    }
+
 }
--- src/main/java/ru/gb/mmick/homework2/Task2.java
+++ src/main/java/ru/gb/mmick/homework2/Task2.java
@@ -1,4 +1,38 @@
 package ru.gb.mmick.homework2;
 
+import java.sql.SQLOutput;
+
 public class Task2 {
+    public static void main(String[] args) {
+
+        summa1();
+        value1();
+        String a = "You!";
+        pr("Hi, Mick!");
+        pr(a);
+
+    }
+
+    public static void summa1() {
+        int a = 0;
+        int b = 12;
+        if (a + b >= 10 && a + b <= 20) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+
+    public static void pr(String b) {
+        System.out.println(b);
+    }
+
+    public static int value1(int x) {
+        int x = 5;
+        if (x < 0) {
+            System.out.println(x + " - число отрицательное");
+        } else {
+            System.out.println(x + " - число положительное");
+        }
+    }
 }
