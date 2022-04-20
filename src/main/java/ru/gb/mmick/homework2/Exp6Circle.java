16cc02e5fdf6c34f4573d62a9beb2d8e8d2a039a
mmickoff <mickmail2000@gmail.com>
2022-04-20 04:23:41 +0300

    220420.0033.hw2.Task2&Exp.v05.Finish.01

--- src/main/java/ru/gb/mmick/homework2/Exp6Circle.java
+++ src/main/java/ru/gb/mmick/homework2/Exp6Circle.java
@@ -1,6 +1,6 @@
 package ru.gb.mmick.homework2;
 
-public class Lesson2Circle {
+public class Exp6Circle {
     public static void main(String[] args) {
         double r1 = 2.1, r2 = 3.12;
         double s1 = calcSR(r1);
@@ -11,6 +11,7 @@ public class Lesson2Circle {
 
     public static double calcSR(double r) {
         double s = 3.14 * r * r;
+      //  System.out.println(s); - начинают печататься плюс ещё площади отдельно
         return s;
     }
     public static void print (double a){
