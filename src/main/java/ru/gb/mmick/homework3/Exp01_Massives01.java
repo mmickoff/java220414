55409fe95a002f14edd5411b587fdd1b1e25d96a
mmickoff <mickmail2000@gmail.com>
2022-04-24 02:07:18 +0300

    220424.01:57-hw3-Task3&Exp-Exp.00&01-v.01

--- src/main/java/ru/gb/mmick/homework3/Exp01_Massives01.java
+++ src/main/java/ru/gb/mmick/homework3/Exp01_Massives01.java
@@ -7,11 +7,12 @@ public class Exp01_Massives01 {
         b[0][1] = 4;
         for (int i = 0; i < b.length; i++) {
             System.out.print("b [" + i + "] - ");
-            for (int j = 0; (j + 1) < b[i].length; j++) {
+            for (int j = 0; j < b[i].length - 1; j++) {
                 System.out.print(b[i][j] + ", ");
             }
-            for (int j = b[i].length - 1; (j + 1) == b[i].length; j++) {
+            for (int j = b[i].length - 1; j < b[i].length; ) {
                 System.out.print(b[i][j] + "; ");
+                break;
             }
 
             System.out.println();
