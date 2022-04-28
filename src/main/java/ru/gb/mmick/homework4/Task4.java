54c9074abb70cfba1d3849d48a4aea1ae7a3906a
mmickoff <mickmail2000@gmail.com>
2022-04-28 21:19:00 +0300

    220428.20:30-hw4-Task4-v.02

--- src/main/java/ru/gb/mmick/homework4/Task4.java
+++ src/main/java/ru/gb/mmick/homework4/Task4.java
@@ -3,133 +3,186 @@ package ru.gb.mmick.homework4;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Task4 {
-    public static void main(String[] args) {
-        initMap();
-        printMap();
+public class MineSweeper {
 
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_BLACK = "\u001B[30m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_PURPLE = "\u001B[35m";
+    private static final String ANSI_CYAN = "\u001B[36m";
+    private static final String ANSI_WHITE = "\u001B[37m";
+
+    private static final int WIDTH = 10;
+    private static final int HEIGHT = 10;
+    private static final int MINE_COUNT = 25;
+
+    private static final int MINE = 1000;
+    private static final int EMPTY = 0;
+    private static final int CELL_OPEN = 1;
+    private static final int CELL_CLOSE = 0;
+    private static final int CELL_FLAG = -1;
+
+    public static void main(String[] args) {
+        final boolean isWin = play();
+        if (isWin) {
+            System.out.println("Поздравляю!\nВы выиграли!");
+        } else {
+            System.out.println("БАБАХ!!!\nВы проиграли!");
+        }
     }
 
-    public class MainClass {
-        public static int SIZE = 3;
-        public static int DOTS_TO_WIN = 3;
-        public static final char DOT_EMPTY = '•';
-        public static final char DOT_X = 'X';
-        public static final char DOT_O = 'O';
-        public static char[][] map;
-        public static Scanner sc = new Scanner(System.in);
-        public static Random rand = new Random();
+    public static boolean play() {
+        int[][] board = generateBoard();
+        int[][] moves = new int[HEIGHT][WIDTH];
+        boolean isPassMove;
+        boolean win;
+        do {
+            isPassMove = move(board, moves);
+            win = isWin(moves);
+        } while (isPassMove && !win);
 
-        public static void main(String[] args) {
-            initMap();
-            printMap();
-            while (true) {
-                humanTurn();
-                printMap();
-                if (checkWin(DOT_X)) {
-                    System.out.println("Победил человек");
-                    break;
-                }
-                if (
-                        isMapFull()) {
-                    System.out.println("Ничья");
-                    break;
-                }
-                aiTurn();
-                printMap();
-                if (checkWin(DOT_O)) {
-                    System.out.println("Победил Искуственный Интеллект");
-                    break;
-                }
-                if (
-                        isMapFull()) {
-                    System.out.println("Ничья");
-                    break;
-                }
-            }
-            System.out.println("Игра закончена");
-        }
+        return isPassMove;
+    }
 
-        public static boolean checkWin(char symb) {
-            if (map[0][0] == symb && map[0][1] == symb && map[0][2] == symb) return
-                    true;
-            if (map[1][0] == symb && map[1][1] == symb && map[1][2] == symb) return
-                    true;
-            if (map[2][0] == symb && map[2][1] == symb && map[2][2] == symb) return
-                    true;
-            if (map[0][0] == symb && map[1][0] == symb && map[2][0] == symb) return
-                    true;
-            if (map[0][1] == symb && map[1][1] == symb && map[2][1] == symb) return
-                    true;
-            if (map[0][2] == symb && map[1][2] == symb && map[2][2] == symb) return
-
-                    true;
-            if (map[0][0] == symb && map[1][1] == symb && map[2][2] == symb) return
-                    true;
-            if (map[2][0] == symb && map[1][1] == symb && map[0][2] == symb) return
-                    true;
-            return false;
-        }
-
-        public static boolean isMapFull() {
-            for (int i = 0; i < SIZE; i++) {
-                for (int j = 0; j < SIZE; j++) {
-                    if (map[i][j] == DOT_EMPTY) return false;
-                }
-            }
-            return true;
-        }
-
-        public static void aiTurn() {
-            int x, y;
-            do {
-                x = rand.nextInt(SIZE);
-                y = rand.nextInt(SIZE);
-            } while (!isCellValid(x, y));
-            System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y +
-                    1));
-            map[y][x] = DOT_O;
-        }
-
-        public static void humanTurn() {
-            int x, y;
-            do {
-                System.out.println("Введите координаты в формате X Y");
-                x = sc.nextInt() - 1;
-                y = sc.nextInt() - 1;
-            } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
-            map[y][x] = DOT_X;
-        }
-
-        public static boolean isCellValid(int x, int y) {
-            if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
-            if (map[y][x] == DOT_EMPTY) return true;
-            return false;
-        }
-
-        public static void initMap() {
-            map = new char[SIZE][SIZE];
-            for (int i = 0; i < SIZE; i++) {
-                for (int j = 0; j < SIZE; j++) {
-                    map[i][j] = DOT_EMPTY;
+    private static boolean isWin(final int[][] moves) {
+        int openCell = 0;
+        for (int[] lines : moves) {
+            for (int cell : lines) {
+                if (cell == CELL_OPEN) {
+                    openCell++;
                 }
             }
         }
+        return openCell == HEIGHT * WIDTH - MINE_COUNT;
+    }
 
-        public static void printMap() {
-            for (int i = 0; i <= SIZE; i++) {
-                System.out.print(i + " ");
+    private static boolean move(final int[][] board, final int[][] moves) {
+        final Scanner scanner = new Scanner(System.in);
+        printBoard(board, moves);
+        while (true) {
+            System.out.print("Ваш ход (строка, столбец, флаг, например А1*): ");
+            String s = scanner.nextLine().toUpperCase(); // приводим к верхнему регистру
+            int row = s.charAt(0) - 'A';
+            int line = s.charAt(1) - '0';
+            if (row >= 0 && row < HEIGHT && line >= 0 && line < WIDTH) {
+                if (s.endsWith("*")) {
+                    moves[line][row] = CELL_FLAG;
+                    return true;
+                }
+                if (isMine(board[line][row])) {
+                    return false;
+                }
+                moves[line][row] = CELL_OPEN;
+                return true;
+            }
+            System.out.println("Неправильный ввод");
+        }
+    }
+
+    private static void printBoard(final int[][] board, final int[][] moves) {
+        System.out.print("   ");
+        for (char i = 'A'; i < 'A' + WIDTH; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println();
+        for (int i = 0; i < HEIGHT; i++) {
+            System.out.printf("%3d", i);
+            for (int j = 0; j < WIDTH; j++) {
+                if (moves[i][j] == CELL_CLOSE) {
+                    System.out.print("[]");
+                    continue;
+                }
+                if (moves[i][j] == CELL_FLAG) {
+                    System.out.print(" P");
+                    continue;
+                }
+                String cellColor = getColorCode(board[i][j]);
+                System.out.print(cellColor);
+                if (board[i][j] == EMPTY) {
+                    System.out.print(" .");
+                } else if (isMine(board[i][j])) {
+                    System.out.print(" *");
+                } else {
+                    System.out.printf("%2d", board[i][j]);
+                }
+                System.out.print(ANSI_RESET);
             }
             System.out.println();
-            for (int i = 0; i < SIZE; i++) {
-                System.out.print((i + 1) + " ");
-                for (int j = 0; j < SIZE; j++) {
-                    System.out.print(map[i][j] + " ");
-                }
-                System.out.println();
+        }
+    }
 
-                System.out.println();
+    private static String getColorCode(final int i) {
+        switch (i) {
+            case EMPTY:
+                return ANSI_WHITE;
+            case MINE:
+                return ANSI_PURPLE;
+            case 1:
+                return ANSI_BLUE;
+            case 2:
+                return ANSI_GREEN;
+            case 3:
+                return ANSI_RED;
+            case 4:
+                return ANSI_CYAN;
+            case 5:
+                return ANSI_YELLOW;
+        }
+        return null;
+    }
+
+    private static int[][] generateBoard() {
+        final int[][] board = fillMines();
+        calculateMines(board);
+        return board;
+    }
+
+    private static int[][] fillMines() {
+        int[][] board = new int[HEIGHT][WIDTH];
+        int mines = MINE_COUNT;
+        final Random random = new Random();
+        while (mines > 0) {
+            int x = random.nextInt(HEIGHT), y = random.nextInt(WIDTH);
+            if (isMine(board[x][y])) {
+                continue;
+            }
+            board[x][y] = MINE;
+            mines--;
+        }
+        return board;
+    }
+
+    private static void calculateMines(int[][] board) {
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                if (isMine(board[i][j])) {
+                    continue;
+                }
+                board[i][j] = getMinesCountAroundCell(board, i, j);
             }
         }
     }
+
+    private static boolean isMine(int i) {
+        return i == MINE;
+    }
+
+    private static int getMinesCountAroundCell(int[][] board, int line, int row) {
+        int mCount = 0;
+        for (int i = line - 1; i <= line + 1; i++) {
+            for (int j = row - 1; j <= row + 1; j++) {
+                if (i < 0 || i >= HEIGHT || j < 0 || j >= WIDTH) {
+                    continue;
+                }
+                if (isMine(board[i][j])) {
+                    mCount++;
+                }
+            }
+        }
+        return mCount;
+    }
+
 }
